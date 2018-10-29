fafdf0fe918a16ea2586a7191c0b814a733108b5
Chunzi Wang <32280134+chunziwang@users.noreply.github.com>
2018-10-29 02:05:42 -0400

    first commit

---
+++ src/h1b_counting.py
@@ -0,0 +1,69 @@
+# create a function to sort by number_certified_applications
+def sortByCount(item):
+    return item[1]
+
+# import csv module
+import csv
+# import regular expression modeule
+import re
+    
+# create dictionaries to hold the count of each job and state in the dataset
+job = dict()
+state = dict()
+    
+# count the number of certified rows
+count = 0
+    
+# read the data
+with open('E:\GitHub\Insight-Data-Engineering-Project\insight_testsuite\tests\test_1\input\h1b_input.csv','r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+        
+    # put data columns into a seperate list
+    fields = csv_reader.next()
+        
+    # find the index of targeted columns using regular expression
+    r = re.compile(".*STATUS")
+    cert_index = fields.index(filter(r.match,fields)[0])
+    r = re.compile(".*SOC_NAME")
+    job_index = fields.index(filter(r.match,fields)[0])
+    r = re.compile(".*WORK.*STATE")
+    state_index = fields.index(filter(r.match,fields)[0])
+        
+    # loop through rows to populate the dictionary
+    for row in csv_reader:
+        if row[cert_index] == 'CERTIFIED':
+            job[row[job_index]] = job.get(row[job_index],0)+1
+            state[row[state_index]] = state.get(row[state_index],0)+1
+            count+=1
+    
+# find the top 10 jobs/states in the dictionary by value
+sorted_count = sorted(state.values())[-10:][::-1]
+sorted_count2 = sorted(job.values())[-10:][::-1]
+    
+# put the selected jobs into a dataframe
+fields = ['TOP_OCCUPATIONS','NUMBER_CERTIFIED_APPLICATIONS','PERCENTAGE'] 
+# calculate the percentage using value/total rows count
+rows = list([key,value,"{0:0.1f}%".format(value*100.0/count)] for key, value in job.iteritems() if value in sorted_count2)
+# sort the rows in descending order
+rows.sort(key=sortByCount,reverse=True)
+filename = "top_10_occupations.txt"
+    
+# write to txt file
+with open(filename, 'w') as csv_file: 
+    csv_writer = csv.writer(csv_file) 
+    csv_writer.writerow(fields) 
+    csv_writer.writerows(rows)
+    
+# put the selected states into a dataframe
+fields = ['TOP_STATES','NUMBER_CERTIFIED_APPLICATIONS','PERCENTAGE'] 
+# calculate the percentage using value/total rows count
+rows = list([key,value,"{0:0.1f}%".format(value*100.0/count)] for key, value in state.iteritems() if value in sorted_count)
+# sort the rows in descending order
+rows.sort(key=sortByCount,reverse=True)
+filename = "top_10_states.txt"
+    
+# write to txt file
+with open(filename, 'w') as csv_file: 
+    csv_writer = csv.writer(csv_file) 
+    csv_writer.writerow(fields) 
+    csv_writer.writerows(rows)
